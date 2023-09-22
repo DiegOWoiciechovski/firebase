0a5ca4cbbaee2c07cbe2d34f121dc86164394822
DiegOWoiciechovski <124810211+DiegOWoiciechovski@users.noreply.github.com>
2023-09-22 16:44:36 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChQUszAQyJG7SWmWd6MFf-zvcKS76aJsU",
+    authDomain: "projeto2-68d7e.firebaseapp.com",
+    projectId: "projeto2-68d7e",
+    storageBucket: "projeto2-68d7e.appspot.com",
+    messagingSenderId: "1043595227442",
+    appId: "1:1043595227442:web:bddf97e323abdb04c3e72b",
+    measurementId: "G-1Q7XTR7B0X"
+  };
+  firebaseConfig.initializeApp(firebaseConfig);
